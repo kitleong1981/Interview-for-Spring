12c6c74f9f7b8c0ba80f7bd26e740ac5f9a6d8d6
kitleong1981 <kitleong1981@users.noreply.github.com>
2022-10-18 12:53:03 -0500

    Show all employees added

--- basic_btn.php
+++ basic_btn.php
@@ -1,4 +1,5 @@
 
-        <div class='col-4 text-center'><a href='?func=show' class='btn btn-success'>Show all companies</a></div>
-        <div class='col-4 text-center'><a href='?func=buildcompany' class='btn btn-info'>Build company info</a></div>
-        <div class='col-4 text-center'><a href='?func=buildemployee' class='btn btn-warning'>Build employee info</a></div>
+        <div class='col-3 text-center'><a href='?func=show' class='btn btn-success'>Show all companies</a></div>
+        <div class='col-3 text-center'><a href='?func=showemployees' class='btn btn-primary'>Show all employees</a></div>
+        <div class='col-3 text-center'><a href='?func=buildcompany' class='btn btn-info'>Build company info</a></div>
+        <div class='col-3 text-center'><a href='?func=buildemployee' class='btn btn-warning'>Build employee info</a></div>
