2d52329f6e211d9c5c51c2272b45daf7f09ab1c5
kitleong1981 <kitleong1981@users.noreply.github.com>
2022-10-18 12:44:56 -0500

    Minor interface update

--- index.php
+++ index.php
@@ -53,9 +53,9 @@ $company_id=(int)$_GET['company_id'];
                                 $sql="select name from employees_db where company=$Id order by name";
                                 $datas2=get_data($sql);
                                 if (empty($datas2)) { echo "<div class='panel-body'>Sorry, this company didn't build any employees...</div></div>\n";} else {
-                                    echo "<div class='panel-heading'>Total employees: ". count($datas2). "</div><div class='panel-body'>";
+                                    echo "<div class='panel-heading'>Total employees: ". count($datas2). "</div><div class='panel-body'><div class='row'>";
                                     for ($j=0; $j<count($datas2);$j++) echo "<div class='col-3'>" . $datas2[$j]["name"] . "</div>";
-                                    echo "</div></div>\n";
+                                    echo "</div></div></div>\n";
                                 }
                                 echo "</div></div></td></tr>";
                             }
